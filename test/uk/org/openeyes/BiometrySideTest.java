1d1b6363dcb2a857857e7650ac6a033eb3861a5e
vetusko <vetusko@gmail.com>
2015-11-26 14:01:51 +0000

    added default test classes

---
+++ test/uk/org/openeyes/BiometrySideTest.java
@@ -0,0 +1,316 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.org.openeyes;
+
+import java.util.ArrayList;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author VEDELEKT
+ */
+public class BiometrySideTest {
+    
+    public BiometrySideTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    /**
+     * Test of setK1 method, of class BiometrySide.
+     */
+    @Test
+    public void testSetK1() {
+        System.out.println("setK1");
+        String BK1 = "";
+        BiometrySide instance = new BiometrySide();
+        instance.setK1(BK1);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getK1 method, of class BiometrySide.
+     */
+    @Test
+    public void testGetK1() {
+        System.out.println("getK1");
+        BiometrySide instance = new BiometrySide();
+        double expResult = 0.0;
+        double result = instance.getK1();
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setK2 method, of class BiometrySide.
+     */
+    @Test
+    public void testSetK2() {
+        System.out.println("setK2");
+        String BK2 = "";
+        BiometrySide instance = new BiometrySide();
+        instance.setK2(BK2);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getK2 method, of class BiometrySide.
+     */
+    @Test
+    public void testGetK2() {
+        System.out.println("getK2");
+        BiometrySide instance = new BiometrySide();
+        double expResult = 0.0;
+        double result = instance.getK2();
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setAxisK1 method, of class BiometrySide.
+     */
+    @Test
+    public void testSetAxisK1() {
+        System.out.println("setAxisK1");
+        String BAxisK1 = "";
+        BiometrySide instance = new BiometrySide();
+        instance.setAxisK1(BAxisK1);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getAxisK1 method, of class BiometrySide.
+     */
+    @Test
+    public void testGetAxisK1() {
+        System.out.println("getAxisK1");
+        BiometrySide instance = new BiometrySide();
+        double expResult = 0.0;
+        double result = instance.getAxisK1();
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setAL method, of class BiometrySide.
+     */
+    @Test
+    public void testSetAL() {
+        System.out.println("setAL");
+        String BAL = "";
+        BiometrySide instance = new BiometrySide();
+        instance.setAL(BAL);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getAL method, of class BiometrySide.
+     */
+    @Test
+    public void testGetAL() {
+        System.out.println("getAL");
+        BiometrySide instance = new BiometrySide();
+        double expResult = 0.0;
+        double result = instance.getAL();
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setSNR method, of class BiometrySide.
+     */
+    @Test
+    public void testSetSNR() {
+        System.out.println("setSNR");
+        String BSNR = "";
+        BiometrySide instance = new BiometrySide();
+        instance.setSNR(BSNR);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getSNR method, of class BiometrySide.
+     */
+    @Test
+    public void testGetSNR() {
+        System.out.println("getSNR");
+        BiometrySide instance = new BiometrySide();
+        double expResult = 0.0;
+        double result = instance.getSNR();
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setLenses method, of class BiometrySide.
+     */
+    @Test
+    public void testSetLenses() {
+        System.out.println("setLenses");
+        String LName = "";
+        BiometrySide instance = new BiometrySide();
+        instance.setLenses(LName);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setLensesName method, of class BiometrySide.
+     */
+    @Test
+    public void testSetLensesName() {
+        System.out.println("setLensesName");
+        String LName = "";
+        Integer LNum = null;
+        BiometrySide instance = new BiometrySide();
+        instance.setLensesName(LName, LNum);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setFormulaName method, of class BiometrySide.
+     */
+    @Test
+    public void testSetFormulaName() {
+        System.out.println("setFormulaName");
+        String LName = "";
+        Integer LNum = null;
+        BiometrySide instance = new BiometrySide();
+        instance.setFormulaName(LName, LNum);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setLenseAConst method, of class BiometrySide.
+     */
+    @Test
+    public void testSetLenseAConst() {
+        System.out.println("setLenseAConst");
+        String LAConst = "";
+        Integer LNum = null;
+        BiometrySide instance = new BiometrySide();
+        instance.setLenseAConst(LAConst, LNum);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setLenseEmmetropia method, of class BiometrySide.
+     */
+    @Test
+    public void testSetLenseEmmetropia() {
+        System.out.println("setLenseEmmetropia");
+        String LEmmetropia = "";
+        Integer LNum = null;
+        BiometrySide instance = new BiometrySide();
+        instance.setLenseEmmetropia(LEmmetropia, LNum);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setLenseIOL method, of class BiometrySide.
+     */
+    @Test
+    public void testSetLenseIOL() {
+        System.out.println("setLenseIOL");
+        String LIOL = "";
+        Integer LNum = null;
+        BiometrySide instance = new BiometrySide();
+        instance.setLenseIOL(LIOL, LNum);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setLenseREF method, of class BiometrySide.
+     */
+    @Test
+    public void testSetLenseREF() {
+        System.out.println("setLenseREF");
+        String LREF = "";
+        Integer LNum = null;
+        BiometrySide instance = new BiometrySide();
+        instance.setLenseREF(LREF, LNum);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getMeasurements method, of class BiometrySide.
+     */
+    @Test
+    public void testGetMeasurements() {
+        System.out.println("getMeasurements");
+        BiometrySide instance = new BiometrySide();
+        ArrayList<BiometryMeasurementData> expResult = null;
+        ArrayList<BiometryMeasurementData> result = instance.getMeasurements();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setMeasurements method, of class BiometrySide.
+     */
+    @Test
+    public void testSetMeasurements() {
+        System.out.println("setMeasurements");
+        ArrayList<BiometryMeasurementData> LMeasurements = null;
+        BiometrySide instance = new BiometrySide();
+        instance.setMeasurements(LMeasurements);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of isSideHaveData method, of class BiometrySide.
+     */
+    @Test
+    public void testIsSideHaveData() {
+        System.out.println("isSideHaveData");
+        BiometrySide instance = new BiometrySide();
+        boolean expResult = false;
+        boolean result = instance.isSideHaveData();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of printBiometrySide method, of class BiometrySide.
+     */
+    @Test
+    public void testPrintBiometrySide() {
+        System.out.println("printBiometrySide");
+        BiometrySide instance = new BiometrySide();
+        instance.printBiometrySide();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
---
+++ test/uk/org/openeyes/StudyDataTest.java
@@ -0,0 +1,353 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.org.openeyes;
+
+import java.util.Calendar;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author VEDELEKT
+ */
+public class StudyDataTest {
+    
+    public StudyDataTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    /**
+     * Test of setStudyDateTime method, of class StudyData.
+     */
+    @Test
+    public void testSetStudyDateTime() {
+        System.out.println("setStudyDateTime");
+        String SDateTime = "";
+        StudyData instance = new StudyData();
+        instance.setStudyDateTime(SDateTime);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getStudyDateTime method, of class StudyData.
+     */
+    @Test
+    public void testGetStudyDateTime() {
+        System.out.println("getStudyDateTime");
+        StudyData instance = new StudyData();
+        Calendar expResult = null;
+        Calendar result = instance.getStudyDateTime();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setPhysicianName method, of class StudyData.
+     */
+    @Test
+    public void testSetPhysicianName() {
+        System.out.println("setPhysicianName");
+        String SPhysicianName = "";
+        StudyData instance = new StudyData();
+        instance.setPhysicianName(SPhysicianName);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setSurgeonName method, of class StudyData.
+     */
+    @Test
+    public void testSetSurgeonName() {
+        System.out.println("setSurgeonName");
+        String SSurgeonName = "";
+        StudyData instance = new StudyData();
+        instance.setSurgeonName(SSurgeonName);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getSurgeonName method, of class StudyData.
+     */
+    @Test
+    public void testGetSurgeonName() {
+        System.out.println("getSurgeonName");
+        StudyData instance = new StudyData();
+        String expResult = "";
+        String result = instance.getSurgeonName();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setFormulaName method, of class StudyData.
+     */
+    @Test
+    public void testSetFormulaName() {
+        System.out.println("setFormulaName");
+        String SFormulaName = "";
+        StudyData instance = new StudyData();
+        instance.setFormulaName(SFormulaName);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getFormulaName method, of class StudyData.
+     */
+    @Test
+    public void testGetFormulaName() {
+        System.out.println("getFormulaName");
+        StudyData instance = new StudyData();
+        String expResult = "";
+        String result = instance.getFormulaName();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setLenseName method, of class StudyData.
+     */
+    @Test
+    public void testSetLenseName() {
+        System.out.println("setLenseName");
+        String SLenseName = "";
+        StudyData instance = new StudyData();
+        instance.setLenseName(SLenseName);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getLenseName method, of class StudyData.
+     */
+    @Test
+    public void testGetLenseName() {
+        System.out.println("getLenseName");
+        StudyData instance = new StudyData();
+        String expResult = "";
+        String result = instance.getLenseName();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setInstituionName method, of class StudyData.
+     */
+    @Test
+    public void testSetInstituionName() {
+        System.out.println("setInstituionName");
+        String SInstitutionName = "";
+        StudyData instance = new StudyData();
+        instance.setInstituionName(SInstitutionName);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setStationName method, of class StudyData.
+     */
+    @Test
+    public void testSetStationName() {
+        System.out.println("setStationName");
+        String SStationName = "";
+        StudyData instance = new StudyData();
+        instance.setStationName(SStationName);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getStationName method, of class StudyData.
+     */
+    @Test
+    public void testGetStationName() {
+        System.out.println("getStationName");
+        StudyData instance = new StudyData();
+        String expResult = "";
+        String result = instance.getStationName();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setStudyID method, of class StudyData.
+     */
+    @Test
+    public void testSetStudyID() {
+        System.out.println("setStudyID");
+        String SStudyID = "";
+        StudyData instance = new StudyData();
+        instance.setStudyID(SStudyID);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getStudyID method, of class StudyData.
+     */
+    @Test
+    public void testGetStudyID() {
+        System.out.println("getStudyID");
+        StudyData instance = new StudyData();
+        String expResult = "";
+        String result = instance.getStudyID();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setStudyInstanceID method, of class StudyData.
+     */
+    @Test
+    public void testSetStudyInstanceID() {
+        System.out.println("setStudyInstanceID");
+        String SStudyInstanceID = "";
+        StudyData instance = new StudyData();
+        instance.setStudyInstanceID(SStudyInstanceID);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getStudyInstanceID method, of class StudyData.
+     */
+    @Test
+    public void testGetStudyInstanceID() {
+        System.out.println("getStudyInstanceID");
+        StudyData instance = new StudyData();
+        String expResult = "";
+        String result = instance.getStudyInstanceID();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of printStudyData method, of class StudyData.
+     */
+    @Test
+    public void testPrintStudyData() {
+        System.out.println("printStudyData");
+        StudyData instance = new StudyData();
+        instance.printStudyData();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getInstituionName method, of class StudyData.
+     */
+    @Test
+    public void testGetInstituionName() {
+        System.out.println("getInstituionName");
+        StudyData instance = new StudyData();
+        String expResult = "";
+        String result = instance.getInstituionName();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setDeviceManufacturer method, of class StudyData.
+     */
+    @Test
+    public void testSetDeviceManufacturer() {
+        System.out.println("setDeviceManufacturer");
+        String SDeviceManufacturer = "";
+        StudyData instance = new StudyData();
+        instance.setDeviceManufacturer(SDeviceManufacturer);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getDeviceManufacturer method, of class StudyData.
+     */
+    @Test
+    public void testGetDeviceManufacturer() {
+        System.out.println("getDeviceManufacturer");
+        StudyData instance = new StudyData();
+        String expResult = "";
+        String result = instance.getDeviceManufacturer();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setDeviceModel method, of class StudyData.
+     */
+    @Test
+    public void testSetDeviceModel() {
+        System.out.println("setDeviceModel");
+        String SDeviceModel = "";
+        StudyData instance = new StudyData();
+        instance.setDeviceModel(SDeviceModel);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getDeviceModel method, of class StudyData.
+     */
+    @Test
+    public void testGetDeviceModel() {
+        System.out.println("getDeviceModel");
+        StudyData instance = new StudyData();
+        String expResult = "";
+        String result = instance.getDeviceModel();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setDeviceSoftwareVersion method, of class StudyData.
+     */
+    @Test
+    public void testSetDeviceSoftwareVersion() {
+        System.out.println("setDeviceSoftwareVersion");
+        String SDeviceSoftwareVersion = "";
+        StudyData instance = new StudyData();
+        instance.setDeviceSoftwareVersion(SDeviceSoftwareVersion);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getDeviceSoftwareVersion method, of class StudyData.
+     */
+    @Test
+    public void testGetDeviceSoftwareVersion() {
+        System.out.println("getDeviceSoftwareVersion");
+        StudyData instance = new StudyData();
+        String expResult = "";
+        String result = instance.getDeviceSoftwareVersion();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
